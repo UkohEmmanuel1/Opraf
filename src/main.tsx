a6f7ed18035a9285c1ac38d9121926472b449d2a
UkohEmmanuel1 <emmanuelukoh08@gmail.com>
2026-05-08 23:48:25 +0100

    uuut

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Import favicon
+import favicon from "./assets/favicon.png";
+
+// Set favicon dynamically
+const link =
+  document.querySelector("link[rel~='icon']") ||
+  document.createElement("link");
+
+link.setAttribute("rel", "icon");
+link.setAttribute("type", "image/png");
+link.setAttribute("href", favicon);
+
+document.head.appendChild(link);
+
 createRoot(document.getElementById("root")!).render(<App />);
