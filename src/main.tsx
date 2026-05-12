7905f50c08fe278902dac45e623fb11377a0eb66
UkohEmmanuel1 <emmanuelukoh08@gmail.com>
2026-05-13 00:43:10 +0100

    Remove broken favicon import

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,9 @@
+
+import "./index.css";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-
-
-// Set favicon dynamically
-const link =
-  document.querySelector("link[rel~='icon']") ||
-  document.createElement("link");
-
-link.setAttribute("rel", "icon");
-link.setAttribute("type", "image/png");
-
-
-document.head.appendChild(link);
-
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <App />
+);
