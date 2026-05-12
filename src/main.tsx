8277bc69521418d84983c1b1800ceb425c9d2af2
UkohEmmanuel1 <emmanuelukoh08@gmail.com>
2026-05-13 00:40:24 +0100

    uuu

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Import favicon
-import favicon from "@/assets/favicon.png";
+
 
 // Set favicon dynamically
 const link =
@@ -12,7 +11,7 @@ const link =
 
 link.setAttribute("rel", "icon");
 link.setAttribute("type", "image/png");
-link.setAttribute("href", favicon);
+
 
 document.head.appendChild(link);
 
